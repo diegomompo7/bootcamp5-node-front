8757f69e20057b164154db7c63d2674496a0b887
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-06-14 13:01:51 +0200

    Estrcutura

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,14 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    }).catch(error => { console.log(error); })
   }
 };
 
