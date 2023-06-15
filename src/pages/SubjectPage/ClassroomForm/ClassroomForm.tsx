d49b483e7f2ebaccff2e39eb6a24ba020c5cb7cb
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-06-15 13:04:23 +0200

    Act

--- src/pages/SubjectPage/ClassroomForm/ClassroomForm.tsx
+++ src/pages/SubjectPage/ClassroomForm/ClassroomForm.tsx
@@ -1,6 +1,5 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { ClassroomCreate } from "../../../models/Classroom";
-import React from "react";
 
 interface ClassroomFormProps {
   userToken?: string;
